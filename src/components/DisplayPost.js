94f1f063cbd4ad5878f31e0d0171d8159b4d4525
Robin Ochoa <robin.ochoa@rokk3rlabs.com>
2020-04-20 10:15:41 -0500

    get list Post console result working

--- src/components/DisplayPost.js
+++ src/components/DisplayPost.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
+import { listPosts } from '../graphql/queries';
+import { API , graphqlOperation } from 'aws-amplify';
 
 class DisplayPosts extends Component {
+
+    componentDidMount = async () => {
+        this.getPosts();
+    }
+
+    getPosts = async() => {
+        const result = await API.graphql(graphqlOperation(listPosts));
+        console.log("All Posts: ", JSON.stringify(result.data.listPosts.items));
+    }
+
     render(){
         return (
             <div>Hello world</div>
