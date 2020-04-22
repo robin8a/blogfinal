c9c768e86761d5188570a349aa21471a7f847565
Robin Ochoa <robin.ochoa@rokk3rlabs.com>
2020-04-22 16:03:03 -0500

    Post Delete and Update (UI and BE) working

--- src/components/DisplayPost.js
+++ src/components/DisplayPost.js
@@ -4,7 +4,7 @@ import { API , graphqlOperation } from 'aws-amplify';
 import DeletePost from './DeletePost'
 import EditPost from './EditPost'
 // import { onCreatePost, onDeletePost, onUpdatePost, onCreateComment, onCreateLike } from '../graphql/subscriptions'
-import { onCreatePost } from '../graphql/subscriptions'
+import { onCreatePost, onDeletePost, onUpdatePost } from '../graphql/subscriptions'
 
 class DisplayPosts extends Component {
     state = {
@@ -23,13 +23,42 @@ class DisplayPosts extends Component {
 
                       this.setState({ posts: updatedPosts});
                  }
-             })
+            });
+
+        this.deletePostListener = API.graphql(graphqlOperation(onDeletePost))
+        .subscribe({
+                next: postData => {
+                    
+                const deletedPost = postData.value.data.onDeletePost
+                const updatedPosts = this.state.posts.filter(post => post.id !== deletedPost.id)
+                this.setState({posts: updatedPosts})
+                }
+        });
+
+        this.updatePostListener = API.graphql(graphqlOperation(onUpdatePost))
+            .subscribe({
+                next: postData => {
+                    const { posts } = this.state
+                    const updatePost = postData.value.data.onUpdatePost
+                    const index = posts.findIndex(post => post.id === updatePost.id) //had forgotten to say updatePost.id!
+                    const updatePosts = [
+                        ...posts.slice(0, index),
+                        updatePost,
+                        ...posts.slice(index + 1)
+                    ]
+
+                    this.setState({ posts: updatePosts})
+
+                }
+        })
+
     }
+    
     
     componentWillUnmount() {
         this.createPostListener.unsubscribe()
-        // this.deletePostListener.unsubscribe()
-        // this.updatePostListener.unsubscribe()
+        this.deletePostListener.unsubscribe()
+        this.updatePostListener.unsubscribe()
         // this.createPostCommentListener.unsubscribe()
         // this.createPostLikeListener.unsubscribe()
     }
@@ -66,7 +95,7 @@ class DisplayPosts extends Component {
                     <br />
                     <span>
                         <DeletePost data={post}/>
-                        <EditPost data={post}/>
+                        <EditPost {...post}/>
                     </span>
                     
                         
